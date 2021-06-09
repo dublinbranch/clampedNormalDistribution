e48d788e4dc6396be863b45c3a81d42b1f82d21e
claudio <claudio@tech.techadsmedia.com>
2021-06-09 10:53:09 +0100

    removed shadowing

--- clampednormaldistribution.cpp
+++ clampednormaldistribution.cpp
@@ -4,8 +4,8 @@ ClampedNormalDistribution::ClampedNormalDistribution(double fixed) {
 	setFixed(fixed);
 }
 
-ClampedNormalDistribution::ClampedNormalDistribution(double _min, double _max, double mean, double stddev) {
-	setParam(_min, _max, mean, stddev);
+ClampedNormalDistribution::ClampedNormalDistribution(double _min, double _max, double _mean, double stddev) {
+	setParam(_min, _max, _mean, stddev);
 }
 
 void ClampedNormalDistribution::setFixed(double _fixed) {
